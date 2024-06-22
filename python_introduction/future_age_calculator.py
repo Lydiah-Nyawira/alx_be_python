327c3517b86f63c79cf07b836a28c7c9aa2df15e
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-06-22 09:02:24 +0300

    Added changes to future_age_calculators.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -4,7 +4,7 @@ current_age = int(input("How old are you?"))
 #Calculate the future age assuming the current year is 2023
 future_year = 2050
 current_year = 2023
-years_to_add = future_year - current_year
+years_to_add = 27
 age = current_age + years_to_add
  
  #Print results
