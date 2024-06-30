700660745f7add4803a3b0f2e9ec622aed21b7a8
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-06-30 12:09:51 +0300

    Add file

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,8 @@
+# Prompt the user
+number = float(input("Enter a number to see its multiplication table: "))
+X = number
+
+# Generate and Print the Multiplication Table:
+for Y in range (1, 11):
+    Z = X * Y
+    print(f"{X} * {Y} = {Z}")
