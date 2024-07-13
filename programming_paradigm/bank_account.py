9f41c56c35a48977ac270f803a038cfefe0bedb2
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-07-13 13:01:06 +0300

    Added changes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,10 +7,10 @@ class BankAccount:
 
     def withdraw(self, amount):
         if self.account_balance >= amount:
-            self_account_balance -= amount
+            self.account_balance -= amount
             return True
         else:
-            self_account_balance = ()
             return False
+        
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")         
+        print(f"Current Balance: ${self.account_balance:.2f}")         
