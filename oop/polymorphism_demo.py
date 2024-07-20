1eb1f22e5f0445690b51a8707f3bd4d6b877ea83
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-07-20 11:36:17 +0300

    Added comments

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -4,11 +4,12 @@ import math
 class Shape:
     def __init__(self):
         pass
-
+    
     def area(self):
+    # This method is expected to be overridden in any subclass of Shape
         raise NotImplementedError
 
- # Derived class - Rectangle   
+# Derived class - Rectangle   
 class Rectangle(Shape):
     def __init__(self,length, width):
         self.length = length
@@ -16,7 +17,8 @@ class Rectangle(Shape):
 
     def area(self):    
         return self.length * self.width
-    
+
+# Derived class -Circle   
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
