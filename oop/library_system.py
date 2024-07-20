cc3b03c26dc0539bf99344fd5538c268e01140da
Lydiah Nyawira <Lydiahnyawira4@gmail.com>
2024-07-20 10:46:05 +0300

    Added comments

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,3 +1,4 @@
+# Base Class - Book
 class Book:
     def __init__(self, title, author):
         self.title = title
@@ -6,6 +7,7 @@ class Book:
     def __str__(self):
         return f"{self.title} by Jane {self.author}"     
 
+# Derived Class - EBook
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title,author)
@@ -14,6 +16,7 @@ class EBook(Book):
     def __str__(self):
         return f"{self.title} by {self.author}, {self.file_size}:"
 
+# Derived Class - PrintBook
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
@@ -21,6 +24,8 @@ class PrintBook(Book):
     
     def __str__(self):
         return f"{self.title} by {self.author}, {self.page_count}:"
+    
+# Manage a collection of books    
 class Library:
     def __init__(self):
         self.books = [] 
